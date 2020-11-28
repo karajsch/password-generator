c8c896e43aed6ecec6cca3329a635276fe5448a5
Kara Schmidt <kara.jane.schmidt@gmail.com>
2020-11-28 17:29:55 -0500

    adding modified script

--- script.js
+++ script.js
@@ -7,8 +7,67 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  var charsLowercase = "abcdefghiklmnopqrstuvwxyz";
+  var charsUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var charsNumbers = "0123456789";
+  var charsSpecial = " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+  var passwordChars = "";
+  var passwordMinLength = 8;
+  var passwordMaxLength = 128;
+  var password = "";
+  var passwordLength = 0;
+  var answer = "";
+
+  passwordLength = prompt(
+    "How many characters do you want in your new password?"
+  );
+
+  if (passwordLength < passwordMinLength) {
+    alert("Your password must contain at least 8 characters.");
+  } else if (passwordLength > passwordMaxLength) {
+    alert("Your password must contain less than 128 characters.");
+  } else {
+    answer = prompt(
+      "Do you want lowercase characters? Press 'y' for yes and 'n' for no."
+    );
+    if (answer === "y" || answer === "Y") {
+      passwordChars = charsLowercase;
+    }
+    answer = prompt(
+      "Do you want uppercase characters? Press 'y' for yes and 'n' for no."
+    );
+    if (answer === "y" || answer === "Y") {
+      passwordChars += charsUppercase;
+    }
+    answer = prompt("Do you want numbers? Press 'y' for yes and 'n' for no.");
+    if (answer === "y" || answer === "Y") {
+      passwordChars += charsNumbers;
+    }
+    answer = prompt(
+      "Do you want special characters? Press 'y' for yes and 'n' for no."
+    );
+    if (answer === "y" || answer === "Y") {
+      passwordChars += charsSpecial;
+    }
+  }
+
+  if (passwordChars === "") {
+    alert("No characters in password");
+    password = "";
+  } else {
+    var i;
+    for (i = 0; i < passwordLength; i++) {
+      password += passwordChars.charAt(
+        Math.floor(Math.random() * passwordChars.length)
+      );
+    }
+  }
+
+  return password;
+}
